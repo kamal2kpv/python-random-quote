2e3ff51f90013ade56fa51e85cd80df5d44635e6
Kamal Verma <Kamal2kpv@yahoo.com>
2020-06-04 23:11:28 +0530

    Explore Python on your own

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,28 @@
 def primarycode():
-   #print("Keep it logically awesome.")
-
+  #print("Keep it logically awesome.")
+  
+  f = open("quotes.txt","a")
+  f.write("\nSpeak like a human !\n")
+  f.write("Keep it Simple!\n")
+  f.write("Simle like a King!")
+  #msg = f.readlines()
+  #print(msg)
+  f.close()
+  
   f = open("quotes.txt")
   quotes = f.readlines()
+  
+  i = len(quotes)
+  j = i - 1
+  k = i - 2
+  #print(i)
+  print ("Read Line: %s" % (quotes[k]).rsplit())
+  print ("Read Line: %s" % (quotes[j]))
+
+  #print(quotes[15])
+  
   f.close()
-
-  print(quotes[13])
-
+  
 if __name__== "__main__":
   primarycode()
+  
